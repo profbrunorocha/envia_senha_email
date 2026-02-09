6948e482b7b75191ab3c3de20637f78bbc42cdad
profbrunorocha <139490635+profbrunorocha@users.noreply.github.com>
2026-02-09 09:09:39 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,3 @@ set -o errexit
 # Instala as dependências
 pip install --upgrade pip
 pip install -r requirements.txt
-
-# Cria as tabelas no banco de dados
-python -c "from app import app, db; app.app_context().push(); db.create_all(); print('✅ Banco de dados inicializado')"
